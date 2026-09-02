fa9f9d434652f0ca984fbb09eaf1bf42eb03a8fd
Eduardo Dieter Wächter <eduardo.d.wachter@gmail.com>
2026-09-02 23:39:07 +0000

    ex3 so o inicio

--- ex3.java
+++ ex3.java
@@ -1,3 +1,12 @@
 public class ex3 {
     
+    public Integer backPack(Integer n, Integer c, Integer itens[][] ){
+        
+        //for pra percorrer a matriz de itens
+        //[peso , valor]
+        for(int i=0;i<n;i++){
+            
+        }
+        return 0;
+    }
 }
